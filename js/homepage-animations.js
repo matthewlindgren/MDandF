d7d5442eeae93cfe1eed707605f401c30d2d2ce2
matthewlindgren <lindmatt@umich.edu>
2024-06-16 14:35:02 -0400

    Finished mobile menu

---
+++ js/homepage-animations.js
@@ -0,0 +1,24 @@
+let menuHidden = true;
+document.querySelector('.burger-button').addEventListener('click', () => {
+    let scrap = document.querySelector('.menu-scrap');
+    let name = document.querySelector('h1');
+    let burger = document.querySelector('.burger-button');
+    if (menuHidden) {
+        menuHidden = false;
+        scrap.style.top = "0.5vh";
+        scrap.style.transform = "rotate(0deg)";
+        setTimeout(function() {
+            name.style.color = "#F7F7F7";
+            burger.src = "./assets/burger-menu-white.svg"
+        }, 500);
+    } else {
+        menuHidden = true;
+        scrap.style.top = "110vh";
+        scrap.style.transform = "rotate(100deg)";
+        name.style.color = "#000000";
+        setTimeout(function() {
+            name.style.color = "#0000000";
+            burger.src = "./assets/burger-menu.svg"
+        }, 100);
+    }
+});
