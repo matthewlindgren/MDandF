e37c6cfabc0d8548962c15a8ae2c9f25ee005dc0
matthewlindgren <lindmatt@umich.edu>
2024-06-16 16:06:02 -0400

    Completed mobile view for contact page

--- js/contact-animations.js
+++ js/contact-animations.js
@@ -8,8 +8,8 @@ document.querySelector('.burger-button').addEventListener('click', () => {
         scrap.style.top = "0.5vh";
         scrap.style.transform = "rotate(0deg)";
         setTimeout(function() {
-            name.style.color = "#F7F7F7";
-            burger.src = "./assets/burger-menu-white.svg"
+            name.style.color = "#000000";
+            burger.src = "./assets/burger-menu.svg"
         }, 500);
     } else {
         menuHidden = true;
@@ -17,8 +17,8 @@ document.querySelector('.burger-button').addEventListener('click', () => {
         scrap.style.transform = "rotate(100deg)";
         name.style.color = "#000000";
         setTimeout(function() {
-            name.style.color = "#0000000";
-            burger.src = "./assets/burger-menu.svg"
+            name.style.color = "#F7F7F7";
+            burger.src = "./assets/burger-menu-white.svg"
         }, 100);
     }
 });
