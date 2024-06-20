00bc61b4f44e6f1830d8ee03ab14e297ad3ba494
matthewlindgren <123006428+matthewlindgren@users.noreply.github.com>
2024-06-20 16:36:28 +0000

    Added swiping effect to header

--- js/text-animations.js
+++ js/text-animations.js
@@ -34,13 +34,15 @@ requestAnimationFrame(raf);
 
 document.addEventListener("DOMContentLoaded", function () {
 	const h1 = document.querySelector('h1');
-	const titleBlock = document.querySelector('.titleBlock');
-
+	const titleBlock = document.querySelector('.title-block');
+	
 	// Get the bounding box of the h1 rectangle
 	const h1Rect = h1.getBoundingClientRect();
 	
 	// Calculate the positions and width
-	const h1Left = h1Rect.left; // or use offsetLeft
+	const h1Left = h1Rect.left;
+	const h1Top = h1Rect.top;
+	const h1Height = h1Rect.bottom - h1Rect.top;
 	const h1Width = h1Rect.width;
 	const h1Right = window.innerWidth - h1Left - h1Width;
 
@@ -51,20 +53,23 @@ document.addEventListener("DOMContentLoaded", function () {
 			@keyframes titleBlock {
 					0% {
 							width: 0;
-							left: ${h1Left}px;
+							left: ${h1Left - 5}px;
 							right: auto;
 					}
 					50% {
-							width: ${h1Width}px;
-							right: ${h1Right}px;
+							width: ${h1Width + 10}px;
+							right: ${h1Right - 5}px;
 					}
 					100% {
 							width: 0;
 							left: auto;
-							right: ${h1Right}px;
+							right: ${h1Right - 5}px;
 					}
 			}
 	`;
+
+	titleBlock.style.height = `${h1Height * 0.98}px`;
+	titleBlock.style.top = `${h1Top - 0.01 * window.innerHeight}px`;
 	
 	// Inject keyframes into the style element
 	style.sheet.insertRule(keyframes, style.sheet.cssRules.length);
